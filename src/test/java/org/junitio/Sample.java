1210cf0f1357034c3a6f6f31a38b566f26dc2c67
Magnus Christensson <magnus.christensson@hotmail.se>
2018-10-13 14:20:58 +0200

    Initial project files commit

---
+++ src/test/java/org/junitio/Sample.java
@@ -0,0 +1,14 @@
+package org.junitio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Sample {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
